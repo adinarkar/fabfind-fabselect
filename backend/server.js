57486fb8104ab21556825821fd0da6eebaf59c1e
Aditya Narkar <aditya.2402263101@vcet.edu.in>
2025-09-01 20:50:16 +0530

    Update server.js
    updated backend port addresss

--- backend/server.js
+++ backend/server.js
@@ -20,5 +20,5 @@ app.use("/api/products", productRoutes);
 // Test route
 app.get("/", (req, res) => res.send("Backend running!"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
