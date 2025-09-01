d366c5e4de04751c9b93b5d109cc9e521a6b1461
Aditya Narkar <aditya.2402263101@vcet.edu.in>
2025-09-01 21:40:08 +0530

    Update men-ethnic.jsx
    updt api call

--- src/pages/men-ethnic.jsx
+++ src/pages/men-ethnic.jsx
@@ -7,7 +7,7 @@ export default function MenEthnic(){
 
   useEffect(() => {
  
-    fetch("http://localhost:5000/api/products")
+    fetch("https://fabfind-fabselect-1-backend.onrender.com")
       .then((res) => res.json())
       .then((data) => {
         // Assuming your product schema has a `category` field
